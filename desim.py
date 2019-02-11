cfa42b314cac9876d6288088a0ef1bdb85f2eda4
Akira Endo <a.endo@tudelft.nl>
2019-02-11 06:46:48 +0100

    New function `deshima_sensitivity2()` that exports in pandas dataframe form. it also calculates the atmospheric transmission inside the function, taking pwv&EL as input parameters.

--- desim.py
+++ desim.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 import copy
 import pandas as pd
+import os
 from scipy.interpolate import interp2d
 from matplotlib.backends.backend_pdf import PdfPages
 
@@ -12,6 +13,7 @@ e = 1.60217662 * 10**-19 # electron charge
 c = 299792458.
 Delta_Al = 188 * 10**-6 * e # gap energy of Al
 eta_pb = 0.4
+Al_refl_ohmic_loss = 0.9975
 
 def deshima_sensitivity(
     F = 350.*10.**9.,
@@ -41,7 +43,6 @@ def deshima_sensitivity(
     """
 
     # Ohmic loss
-    Al_refl_ohmic_loss = 0.9975
     eta_M1_ohmic = Al_refl_ohmic_loss
     eta_M2_ohmic = Al_refl_ohmic_loss
 
@@ -133,6 +134,151 @@ def deshima_sensitivity(
 
     return result
 
+def deshima_sensitivity2(
+    F = 350.*10.**9., # Hz, scalar or vector
+    pwv=0.5, # mm, scalar
+    EL = 60., # deg, scalar
+    R = 500, # scalar. R = Q = F/FWHM = F/dF is assumed.
+    eta_M1_spill = 0.99, # scalar or vector
+    eta_M2_spill = 0.90, # scalar or vector
+    eta_wo = 0.99, # scalar or vector. product of all cabin loss (ohmic and spillover)\
+    n_wo_mirrors = 4, # scalar
+    eta_co = 0.65, # scalar or vector. product of co spillover, qo filter transmission
+    eta_lens_antenna_rad = 0.81, # scalar or vector. D2_2V3.pdf, p14: front-to-back ratio 0.93 * reflection efficiency 0.9 * matching 0.98 * antenna spillover 0.993
+    eta_circuit = 0.45, # scalar or vector. 'Alejandro Efficiency', from the feedpoint of the antenna to being absorbed in the KID.
+    theta_maj = 22. * np.pi / 180. / 60. / 60., # scalar or vector.
+    theta_min = 22. * np.pi / 180. / 60. / 60., # scalar or vector.
+    eta_mb = 0.6, # scalar or vector.
+    Tb_cmb = 2.725, # scalar
+    Tp_amb = 273., # scalar
+    Tp_cabin = 290., # scalar
+    Tp_co = 4., # scalar
+    Tp_chip = 0.12, # scalar
+    snr = 5.,
+    integ_hours = 10.
+    ):
+
+    eta_M1_ohmic = Al_refl_ohmic_loss
+    eta_M2_ohmic = Al_refl_ohmic_loss
+
+    # Collect efficiencies at the same temperature
+    eta_M1 =  eta_M1_ohmic * eta_M1_spill
+    eta_chip = eta_lens_antenna_rad * eta_circuit
+
+    # Forward efficiency
+    eta_forward = eta_M1*eta_M2_ohmic * eta_M2_spill * eta_wo + (1-eta_M2_spill)*eta_wo # does/should not include window loss
+
+    # Equivalent Bandwidth of 1 channel, assuming Lorentzian
+    # Note that this calculates the coupling to a continuum source.
+    W_F = 0.5*np.pi*F/R
+
+    # Calcuate eta. scalar/vector depending on F.
+    eta_atm = eta_atm_func(F=F, pwv=pwv, EL=EL)
+
+    # Calculate the intrinsic Johnson-Nyquist power for all physical temperatures
+    Pjn_cmb     = jn(F=F,T=Tb_cmb)
+    Pjn_amb     = jn(F=F,T=Tp_amb)
+    Pjn_cabin   = jn(F=F,T=Tp_cabin)
+    Pjn_co      = jn(F=F,T=Tp_co)
+    Pjn_chip    = jn(F=F,T=Tp_chip)
+
+    # Power density (W/Hz) at different stages
+    PF_sky       = rad_trans(Pjn_cmb,   Pjn_amb,   eta_atm)
+    PF_M1        = rad_trans(PF_sky,      Pjn_amb,   eta_M1)
+    PF_M2_spill  = rad_trans(PF_M1,       PF_sky,     eta_M2_spill)
+    PF_M2        = rad_trans(PF_M2_spill, Pjn_amb,   eta_M2_ohmic)
+    PF_wo        = rad_trans(PF_M2,       Pjn_cabin, eta_wo)
+    [PF_window, eta_window] = window_trans(PF_wo,    Pjn_cabin,  Pjn_co, F)
+    PF_co        = rad_trans(PF_window,   Pjn_co,    eta_co)
+    PF_KID       = rad_trans(PF_co,       Pjn_chip,  eta_chip) # Power density _absorbed_ by the KID
+
+    # Instrument optical efficiency
+    eta_inst = eta_chip * eta_co * eta_window
+
+    eta_a = aperture_efficiency(
+        theta_maj = theta_maj,
+        theta_min = theta_min,
+        eta_mb = eta_mb,
+        F = F
+        )
+
+    eta_pol = 0.5
+    eta_sw = eta_pol * eta_atm * eta_a * eta_forward
+
+    # Photon + R(ecombination) NEP
+    P_KID = PF_KID * W_F
+    photon_term = 2 * P_KID * (h*F + P_KID/W_F )
+    r_term = 4 * Delta_Al * P_KID / eta_pb
+    NEP_KID = np.sqrt(photon_term + r_term) # KID NEP
+    NEP_inst = NEP_KID / eta_inst # Instrument NEP
+
+    NEFD_ = NEFD(NEP_inst,
+         eta_source_window = eta_sw,
+         F = F,
+         Q = R
+        )
+
+    NEF = NEFD_ * W_F
+    MDLF = NEF * snr / np.sqrt(integ_hours*60.*60.)
+
+    Trx = NEP_inst/k/np.sqrt(2*W_F) - T_CW(F,PF_wo)
+
+    # Make Pandas DataFrame out of the result
+
+    result = pd.concat([
+        pd.Series(F,name='F'),
+        pd.Series(eta_atm,name='eta_atm'),
+        pd.Series(R,name='R'),
+        pd.Series(W_F,name='W_F'),
+        pd.Series(eta_a,name='eta_a'),
+        pd.Series(eta_forward,name='eta_forward'),
+        pd.Series(eta_sw,name='eta_sw'),
+        pd.Series(eta_window,name='eta_window'),
+        pd.Series(eta_inst,name='eta_inst'),
+        pd.Series(eta_circuit,name='eta_circuit'),
+        pd.Series(T_CW(F,PF_sky),name='Tb_sky'),
+        pd.Series(T_CW(F,PF_M1),name='Tb_M1'),
+        pd.Series(T_CW(F,PF_M2),name='Tb_M2'),
+        pd.Series(T_CW(F,PF_wo),name='Tb_wo'),
+        pd.Series(T_CW(F,PF_window),name='Tb_window'),
+        pd.Series(T_CW(F,PF_co),name='Tb_co'),
+        pd.Series(T_CW(F,PF_KID),name='Tb_KID'),
+        pd.Series(P_KID,name='P_KID (W)'),
+        pd.Series(NEP_KID,name='NEP_KID'),
+        pd.Series(NEP_inst,name='NEP_inst'),
+        pd.Series(NEFD_*10**26,name='NEFD'),
+        pd.Series(NEF,name='NEF'),
+        pd.Series(MDLF,name='MDLF'),
+        pd.Series(snr,name='SNR'),
+        pd.Series(integ_hours,name='integ_hours'),
+        pd.Series(Trx,name='Equivalent Trx'),
+        ],axis=1
+        )
+
+    # Turn Scalar values into vectors
+    result = result.fillna(method='ffill')
+
+    return result
+
+# Calculate eta_atm as a function of F
+# path https://qiita.com/ymdymd/items/d758110d429f72bc10fb
+
+# Make a function (eta_atm_func_zenith) by interpolation.
+# The function calculates the atmospheric transmission (eta_atm) from:
+# 1) PWV, 2) frequency 3) elevation.
+
+
+def eta_atm_func(F, pwv, EL=60.):
+        if np.average(F) > 10.**9:
+            F = F / 10.**9
+        eta_atm_df = pd.read_csv(os.path.dirname(__file__)+'/data/atm.csv',skiprows=4,delim_whitespace=True,header=0)
+        eta_atm_func_zenith = eta_atm_interp(eta_atm_df)
+        eta_atm = np.abs(eta_atm_func_zenith(pwv, F)) ** (1./np.sin(EL*np.pi/180.))
+        if len(eta_atm)==1:
+            eta_atm = eta_atm[0]
+        else:
+            eta_atm = eta_atm[:,0]
+        return eta_atm
 
 def eta_atm_interp(eta_atm_dataframe):
     """
