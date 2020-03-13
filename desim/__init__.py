66ceae9a1d386a825e1bfb8b736aa86499ef7c39
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-03-13 10:41:05 +0900

    Add desim package

---
+++ desim/__init__.py
@@ -0,0 +1,15 @@
+"""
+DEshima SIMulator.
+Module for calculating the sensitivity of a DESHIMA-type spectrometer.
+
+FAQ
+-----
+Q.  Where is the point-source coupling phase and amplitude loss
+    due to the mismatch between the
+    beam in radiation and reception, that Shahab calculates
+    at the lens surface?
+        A.  It is included in the main beam efficiency.
+            These losses reduce the coupling to a point source,
+            but the power (in transmission) couples to the sky.
+"""
+
