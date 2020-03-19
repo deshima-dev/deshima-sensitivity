6aab69274af0d55c1891861afad3280c87a2f493
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-03-19 16:14:15 +0900

    #9 Add consts module

---
+++ desim/consts.py
@@ -0,0 +1,12 @@
+# dependent packages
+import numpy as np
+from astropy import constants
+
+
+# constants
+c = constants.c.value
+e = constants.e.value
+h = constants.h.value
+k = constants.k_B.value
+pi = np.pi
+two_pi = 2 * np.pi
