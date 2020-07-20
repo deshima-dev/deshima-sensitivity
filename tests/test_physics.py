6e10d2278ca8d6c8b00daabb7b29ab0b90336255
戸上陽平 <yohei@MacBook-Pro-3.local>
2020-07-20 19:21:49 +0900

    #27 Check bug

--- tests/test_physics.py
+++ tests/test_physics.py
@@ -1,4 +1,3 @@
-import numpy as np
 from math import isclose
 from deshima_sensitivity import physics
 
@@ -7,4 +6,3 @@ def test_T_from_psd():
     output = physics.T_from_psd(1e11, 1e-26, "Rayleigh-Jeans")
     expected = 0.00072429730  # calculated by python
     assert isclose(output, expected, rel_tol=0.01)
-
