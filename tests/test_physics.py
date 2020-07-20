5b241386bccec463cebe8ae96d4b94c9a0b9a74d
戸上陽平 <yohei@MacBook-Pro-3.local>
2020-07-20 18:51:14 +0900

    #27 Add test function (T_from_psd)

---
+++ tests/test_physics.py
@@ -0,0 +1,8 @@
+from deshima_sensitivity import physics
+
+
+def test_T_from_psd():
+    def test_T_from_psd():
+    output = physics.T_from_psd(1e11, 1e-26, "Rayleigh-Jeans")
+    expected = 0.00072429730  # calculated by python
+    assert isclose(output, expected, rel_tol=0.01)
