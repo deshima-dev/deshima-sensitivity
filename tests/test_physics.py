701cb548662e1221ae1b65ae92589a781e917c52
Keiichi Matsuda <matsuda@a.phys.nagoya-u.ac.jp>
2020-07-29 16:32:58 +0900

    make johnson nyquist psd function and psd function

---
+++ tests/test_physics.py
@@ -0,0 +1,15 @@
+import numpy as np
+from math import isclose
+from deshima_sensitivity import physics
+
+
+def test_johnson_nyquist_psd():
+    expected = 1.3833132 * 10 ** -21
+    output = johnson_nyquist_psd(10, 100)
+    assert output == expected
+
+
+def test_nph():
+    expected = 2.0876827 * 10 ** 11
+    output = nph(10, 100)
+    assert output == expected
